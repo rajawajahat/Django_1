354b47eab415825729669be5176a7ccd5a24cea0
Raja Wajahat Hussain <whussain@timexperts.com.pk>
2021-06-14 17:35:24 +0500

    first tutorial

--- Django_Board/Django_Board/urls.py
+++ Django_Board/Django_Board/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from board import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homepage/', views.home, name='home')
 ]
